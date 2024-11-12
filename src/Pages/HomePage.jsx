b6cf4b6003ac92e456a20c12f13819398d218051
siraajul <shirajulislamparvez@gmail.com>
2024-11-12 23:37:21 +0600

    Wrap with Layout

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,5 +1,11 @@
+import LayOut from "../Layout/Layout";
+
 const HomePage = () => {
-  return <div></div>;
+  return (
+    <LayOut>
+      <h1 className="bg-green-300">This is HomePage</h1>
+    </LayOut>
+  );
 };
 
 export default HomePage;
