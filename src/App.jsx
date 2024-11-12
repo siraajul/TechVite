a0b21aa93e9965847420a4286276cd97972ef0fa
siraajul <shirajulislamparvez@gmail.com>
2024-11-12 18:14:08 +0600

    Add:--> Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
 import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import "./index.css";
+import HomePage from "./Pages/HomePage";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={HomePage} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
