843080a43b71dda8387ee8cc63d0e35a089b879b
siraajul <shirajulislamparvez@gmail.com>
2024-11-12 23:37:13 +0600

    Wrap with Layout

--- src/Pages/DetailsPage.jsx
+++ src/Pages/DetailsPage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import LayOut from "../Layout/Layout";
 
 const DetailsPage = () => {
-  return <div></div>;
+  return (
+    <LayOut>
+      <h1>This is Post Detils Page</h1>
+    </LayOut>
+  );
 };
 
 export default DetailsPage;
