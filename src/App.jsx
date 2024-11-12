80249d45f1cb4a367afad7fbc5fd5a922376be84
siraajul <shirajulislamparvez@gmail.com>
2024-11-12 23:36:17 +0600

    Update:Routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import HomePage from "./Pages/HomePage";
+import ByCategoryPage from "./Pages/ByCategoryPage";
+import DetailsPage from "./Pages/DetailsPage";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={HomePage} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Category/:categoryID" element={<ByCategoryPage />} />
+        <Route path="/Details/:postID" element={<DetailsPage />} />
       </Routes>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
