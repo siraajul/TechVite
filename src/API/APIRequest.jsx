78bdeba36394125bd5d195725ae0c35a26066956
siraajul <shirajulislamparvez@gmail.com>
2024-11-13 08:28:38 +0600

    Reformat:--> API Request

--- src/API/APIRequest.jsx
+++ src/API/APIRequest.jsx
@@ -4,40 +4,40 @@ const BaseURL = "https://basic-blog.teamrabbil.com/api";
 
 //Post Categories
 export async function postCategories() {
-  let res = await axios.get(BaseURL + "/post-categories");
-  if (res.status == 200) {
-    return res.data;
-  } else {
-    [];
-  }
+    let res = await axios.get(BaseURL + "/post-categories");
+    if (res.status == 200) {
+        return res.data;
+    } else {
+        [];
+    }
 }
 
 //Post Latest
 export async function postLatest() {
-  let res = await axios.get(BaseURL + "/post-newest");
-  if (res.status == 200) {
-    return res.data;
-  } else {
-    [];
-  }
+    let res = await axios.get(BaseURL + "/post-newest");
+    if (res.status == 200) {
+        return res.data;
+    } else {
+        [];
+    }
 }
 
 //Post Lists
 export async function postByCategory(id) {
-  let res = await axios.get(BaseURL + "/post-list" + id);
-  if (res.status == 200) {
-    return res.data;
-  } else {
-    [];
-  }
+    let res = await axios.get(BaseURL + "/post-list" + id);
+    if (res.status == 200) {
+        return res.data;
+    } else {
+        [];
+    }
 }
 
 //Post Details
 export async function postDetails(id) {
-  let res = await axios.get(BaseURL + "/post-details" + id);
-  if (res.status == 200) {
-    return res.data;
-  } else {
-    [];
-  }
+    let res = await axios.get(BaseURL + "/post-details" + id);
+    if (res.status == 200) {
+        return res.data;
+    } else {
+        [];
+    }
 }
