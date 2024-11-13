784fe73d09771d0bf7b2d7e6a7875d21466c7252
siraajul <shirajulislamparvez@gmail.com>
2024-11-14 05:47:13 +0600

    Update: HomePage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,7 +3,7 @@ import LayOut from "../Layout/Layout";
 const HomePage = () => {
   return (
     <LayOut>
-      <h1 className="bg-green-300">This is HomePage</h1>
+      <h1>This is HomePage</h1>
     </LayOut>
   );
 };
