751055732e26b1d17eb3c84225f1c206976c5b39
siraajul <shirajulislamparvez@gmail.com>
2024-11-13 08:28:47 +0600

    Reformat:--> App

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import { useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 import HomePage from "./Pages/HomePage";
 import ByCategoryPage from "./Pages/ByCategoryPage";
 import DetailsPage from "./Pages/DetailsPage";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/Category/:categoryID" element={<ByCategoryPage />} />
-        <Route path="/Details/:postID" element={<DetailsPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/Category/:categoryID" element={<ByCategoryPage/>}/>
+                <Route path="/Details/:postID" element={<DetailsPage/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default App;
