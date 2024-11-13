c9f96c8a140c98485491192f417d7ef9a239d6ec
siraajul <shirajulislamparvez@gmail.com>
2024-11-14 05:46:44 +0600

    Add: API Manipulation

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,82 +1,68 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {postCategories} from "../API/APIRequest.jsx";
+import {NavLink} from "react-router-dom";
 
 const LayOut = (props) => {
-  return (
-    <div>
-      <div className="navbar bg-base-100">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
+
+    const [categories, SetCategories] = useState([]);
+
+    useEffect(() => {
+
+        (async () => {
+            let res = await postCategories();
+            SetCategories(res);
+        })()
+
+    }, []);
+
+    return (
+        <div>
+            <div className="navbar shadow bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16"
+                                />
+                            </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+                        >
+                            {
+                                categories.map((item, index) => {
+                                    return <li><NavLink to={"/Category/"+item['id']}>{item['name']}</NavLink></li>
+                                })
+                            }
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost text-xl">TechVite</a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {
+                            categories.map((item, index) => {
+                                return <li><NavLink to={"/Category/"+item['id']}>{item['name']}</NavLink></li>
+                            })
+                        }
+                    </ul>
+                </div>
             </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
-            </ul>
-          </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+            {/* eslint-disable-next-line react/prop-types */}
+            {props.children}
         </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
-        </div>
-        <div className="navbar-end">
-          <a className="btn">Button</a>
-        </div>
-      </div>
-      {props.children}
-    </div>
-  );
+    );
 };
 
 export default LayOut;
