9f5bcdff01b678f19822ad9d30bbe8b97b433cd9
siraajul <shirajulislamparvez@gmail.com>
2024-11-14 06:37:11 +0600

    Reformat:--> Code

--- src/Components/BlogDetails.jsx
+++ src/Components/BlogDetails.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import LayOut from "../Layout/Layout";
 
 const BlogDetails = () => {
-  return (
-    <LayOut>
-      <h1>This is post by category page</h1>
-    </LayOut>
-  );
+    return (
+        <LayOut>
+            <h1>This is post by category page</h1>
+        </LayOut>
+    );
 };
 
 export default BlogDetails;
