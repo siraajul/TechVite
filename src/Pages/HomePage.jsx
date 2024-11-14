2f2db0573ef3d332b0dd3a6de36982450bb4ecec
siraajul <shirajulislamparvez@gmail.com>
2024-11-14 06:29:08 +0600

    API Call: HomePage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,11 +1,26 @@
 import LayOut from "../Layout/Layout";
+import BlogList from "../Components/BlogList.jsx";
+import {useEffect, useState} from "react";
+import {postLatest} from "../API/APIRequest.jsx";
 
 const HomePage = () => {
-  return (
-    <LayOut>
-      <h1>This is HomePage</h1>
-    </LayOut>
-  );
+
+    let [list, SetList] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            let res = await postLatest();
+            SetList(res);
+        })()
+
+    }, []);
+
+
+    return (
+        <LayOut>
+            <BlogList list={list}/>
+        </LayOut>
+    );
 };
 
 export default HomePage;
