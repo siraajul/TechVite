f48582d3a439d7e88c302d219a743443db6e8df7
siraajul <shirajulislamparvez@gmail.com>
2024-11-14 06:37:17 +0600

    Reformat:--> Code

--- src/Pages/DetailsPage.jsx
+++ src/Pages/DetailsPage.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import LayOut from "../Layout/Layout";
 
 const DetailsPage = () => {
-  return (
-    <LayOut>
-      <h1>This is Post Detils Page</h1>
-    </LayOut>
-  );
+    return (
+        <LayOut>
+            <h1>This is Post Detils Page</h1>
+        </LayOut>
+    );
 };
 
 export default DetailsPage;
