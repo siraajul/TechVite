2365f10787d4e7b3441c734c8f32639747920165
siraajul <shirajulislamparvez@gmail.com>
2024-11-14 06:37:14 +0600

    Reformat:--> Code

--- src/Pages/ByCategoryPage.jsx
+++ src/Pages/ByCategoryPage.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import LayOut from "../Layout/Layout";
 
 const ByCategoryPage = () => {
-  return (
-    <LayOut>
-      <div>
-        <h1>Category Page</h1>
-      </div>
-    </LayOut>
-  );
+    return (
+        <LayOut>
+            <div>
+                <h1>Category Page</h1>
+            </div>
+        </LayOut>
+    );
 };
 
 export default ByCategoryPage;
