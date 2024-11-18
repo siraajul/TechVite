fcb0d888515053606e1258fefae3335b6793cbef
siraajul <shirajulislamparvez@gmail.com>
2024-11-18 08:30:33 +0600

    Update Footer to BrowserRoute

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import ByCategoryPage from "./Pages/ByCategoryPage";
 import DetailsPage from "./Pages/DetailsPage";
+import Footer from "./Components/Footer.jsx";
 
 const App = () => {
     return (
@@ -12,6 +13,7 @@ const App = () => {
                 <Route path="/Category/:categoryID" element={<ByCategoryPage/>}/>
                 <Route path="/Details/:postID" element={<DetailsPage/>}/>
             </Routes>
+            <Footer/>
         </BrowserRouter>
     );
 };
