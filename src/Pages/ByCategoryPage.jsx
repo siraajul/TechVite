11501d8f48d3124318239df6157d1755609f67b0
siraajul <shirajulislamparvez@gmail.com>
2024-11-18 22:17:47 +0600

    Update--> ByCategoryPage

--- src/Pages/ByCategoryPage.jsx
+++ src/Pages/ByCategoryPage.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import LayOut from "../Layout/Layout";
+import {useParams} from "react-router-dom";
+import {postByCategory,} from "../API/APIRequest.jsx";
+import Loader from "../Components/Loader.jsx";
+import BlogList from "../Components/BlogList.jsx";
 
 const ByCategoryPage = () => {
+    let {categoryID} = useParams();
+
+    let [list, SetList] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            let res = await postByCategory();
+            SetList(res);
+        })()
+
+    }, []);
+
     return (
         <LayOut>
             <div>
-                <h1>Category Page</h1>
+                {list === null ? <Loader/> : <BlogList list={list}/>}
             </div>
         </LayOut>
     );
